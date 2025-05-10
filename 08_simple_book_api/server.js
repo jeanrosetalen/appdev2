bb0b7fc66dabd5a4a846abce97a683b2c9be4e0e
jeanrosetalen <rjean5097@gmail.com>
2025-05-10 12:00:20 +0800

    Initialize express module

---
+++ 08_simple_book_api/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+const books = [
+    {
+        id: 1,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee"
+    },
+    {
+        id: 2,
+        "title":  "1984",
+        "author": "George Orwell"
+    },
+    {
+        id: 3,
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen"
+    },
+    {
+        id: 4,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald"
+    },
+    {
+        id: 5,
+        "title": "Moby-Dick",
+        "author": "Herman Melville"
+    }
+]
+
+
+
